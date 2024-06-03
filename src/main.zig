fc8a39fff26050ab5636245bfc85e56d85e033a3
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2024-06-02 18:43:07 -0700

    chore: update to zig version 0.13.0-dev.344+b2588de6c

--- src/main.zig
+++ src/main.zig
@@ -87,7 +87,7 @@ pub fn main() !void {
                 try argv.appendSlice(&.{ "-o", gen_path_full });
                 try argv.append(filename);
                 std.log.debug("argv={s}\n", .{argv.items});
-                const exec_res = try std.ChildProcess.run(.{ .allocator = alloc, .argv = argv.items });
+                const exec_res = try std.process.Child.run(.{ .allocator = alloc, .argv = argv.items });
                 // std.debug.print("term={}\n", .{exec_res.term});
                 // std.debug.print("stderr={s}\n", .{exec_res.stderr});
                 // std.debug.print("stdout={s}\n", .{exec_res.stdout});
